d78b2353e980dfddebdbb3d78cf8fa862ac57151
Hugo de Lelis <hugodelelis00@gmail.com>
2024-10-21 20:38:15 -0300

    add footer

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -4,11 +4,12 @@ import { AboutComponent } from '../../components/about/about.component';
 import { StacksComponent } from '../../components/stacks/stacks.component';
 import { ProjectsComponent } from '../../components/projects/projects.component';
 import { ContactComponent } from '../../components/contact/contact.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [HeaderComponent, AboutComponent, StacksComponent, ProjectsComponent, ContactComponent],
+  imports: [HeaderComponent, AboutComponent, StacksComponent, ProjectsComponent, ContactComponent, FooterComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
