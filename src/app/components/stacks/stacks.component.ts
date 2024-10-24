b59d0dea5c58e3c92cb5887ff3e98c54b8446fb2
Hugo de Lelis <hugodelelis00@gmail.com>
2024-10-24 20:23:52 -0300

    finish themes

--- src/app/components/stacks/stacks.component.ts
+++ src/app/components/stacks/stacks.component.ts
@@ -1,5 +1,4 @@
 import { Component, signal } from '@angular/core';
-import { ITechStack } from '../../interfaces/stacks.interface';
 
 @Component({
   selector: 'app-stacks',
@@ -9,26 +8,5 @@ import { ITechStack } from '../../interfaces/stacks.interface';
   styleUrl: './stacks.component.scss'
 })
 export class StacksComponent {
-  techStacks = signal<ITechStack[]>([
-    {
-      src: 'assets/icons/angular.svg',
-      alt: 'angular',
-      title: 'Angular',
-    },
-    {
-      src: 'assets/icons/typescript.svg',
-      alt: 'typescript',
-      title: 'TypeScript',
-    },
-    {
-      src: 'assets/icons/bootstrap.svg',
-      alt: 'bootstrap',
-      title: 'Bootstrap',
-    },
-    {
-      src: 'assets/icons/sass.svg',
-      alt: 'sass',
-      title: 'Sass',
-    }
-  ])
+
 }
